c54bfad7df81bf21f694cb279049adf7a34b0049
idhan19 <wildan.fr002@gmail.com>
2023-10-31 18:37:05 +0700

    Initial commit

---
+++ app/src/main/java/com/example/restoranceban/MainActivity.kt
@@ -0,0 +1,40 @@
+package com.example.restoranceban
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Spinner
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val spinner: Spinner = findViewById(R.id.spinner)
+        ArrayAdapter.createFromResource(
+            this,
+            R.array.location,
+            android.R.layout.simple_spinner_item
+        ).also { adapter ->
+            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+            spinner.adapter = adapter
+        }
+
+        val btnSubmit = findViewById<Button>(R.id.submitButton)
+        btnSubmit?.setOnClickListener {
+            val selectedLocation = spinner.selectedItem.toString()
+
+            val inputNama = findViewById<EditText>(R.id.name)
+            val nama = inputNama.text.toString()
+
+            val intent = Intent(this, Tampilan2::class.java)
+            intent.putExtra("NAMA", nama)
+            intent.putExtra("LOCATION", selectedLocation)
+            startActivity(intent)
+        }
+
+    }
+}
+
---
+++ app/src/main/java/com/example/restoranceban/tampilan4.kt
@@ -0,0 +1,11 @@
+package com.example.restoranceban
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class tampilan4 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_tampilan4)
+    }
+}
